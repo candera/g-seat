d852ee8a2ce13f42fe9e3ba62c9a0bd49c754077
Craig Andera <candera@wangdera.com>
2015-10-09 17:37:40 -0400

    Add simple replay functionality.

--- scratch.clj
+++ scratch.clj
@@ -62,3 +62,33 @@
               0 1 0.25 1.0)
     (scale-to (nonlinear (- base-g g))
               0 1 0.25 0)))
+
+(let [t0 (.getTime (java.util.Date.))]
+  (defn now
+    []
+    (/ (- (.getTime (java.util.Date.))
+          t0)
+       1000.0)))
+
+(defn replay [device path]
+  (let [output *out*
+        t0 (now)]
+   (with-open [ ;;output *out*
+               in (clojure.java.io/reader path)]
+     (loop [tstart nil
+            [line & more] (line-seq in)]
+       (when line
+         (let [[t xdot ydot zdot yaw pitch roll bl br sl sr]
+               (map #(Float. %) (clojure.string/split line #", "))]
+           (when tstart
+             (let [real-elapsed (- (now) t0)
+                   playback-elapsed (- t tstart)]
+               (when (< real-elapsed playback-elapsed)
+                 (Thread/sleep (long (* 1000 (- playback-elapsed real-elapsed)))))))
+           (.write output (str "M BL " bl "\n"))
+           (.write output (str "M BR " br "\n"))
+           (.write output (str "M SL " sl "\n"))
+           (.write output (str "M SR " sr "\n"))
+           (.write output "----------------\n")
+           (.flush output)
+           (recur (or tstart t) more)))))))
