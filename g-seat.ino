fb5e7d21849f8ec472223d9662f1af70fe0c329d
Craig Andera <candera@wangdera.com>
2015-10-12 18:57:04 -0400

    Further attempts to refine control algorithm.

--- g-seat.ino
+++ g-seat.ino
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <stdio.h>
 
 #define DEBUG false
 
@@ -13,6 +14,12 @@ enum Direction {
   Left,
   Right
 };
+
+void debug(int i) {
+  if (_logLevel >= Debug) {
+    Serial.print(i);
+  }
+}
 
 void debug(float f) {
   if (_logLevel >= Debug) {
@@ -34,18 +41,22 @@ void debug() {
 
 class DriveUnit {
  private:
-  const int POS_LOW = 780;
+  const int POS_LOW = 800;
   const int POS_HIGH = 1020;
-  const int HISTORY = 1;
   String _channelName;
   int _pinEn;
   int _pinDis;
   int _pinRPWM;
   int _pinLPWM;
   int _pinPos;
-  float _target;
-  int _seeking;
-  int* _vals;
+  int _target;
+  int _priorTarget;
+  long _targetSetAt;
+  int _raw;
+  int _pos;
+  int _smoothedPos;
+  int _drive;
+  bool _seeking;
   long _counter;
   Direction _direction;
 
@@ -57,19 +68,15 @@ class DriveUnit {
     _pinRPWM = pinRPWM;
     _pinLPWM = pinLPWM;
     _pinPos = pinPos;
-    _target = 0.25;
-    _seeking = seeking(_target);
+    _target = 25;
+    _priorTarget = 25;
+    _pos = 25;
+    _smoothedPos = 25;
+    _drive = 0;
     _channelName = channelName;
     _counter = 0;
-    _vals = (int*) malloc(HISTORY * sizeof(int));
-    for (int i = 0; i < HISTORY; ++i) {
-      _vals[i] = _seeking;
-    }
     _direction = direction;
-  }
-
-  int seeking(float target) {
-    return map(target * 1000000, 0, 1000000, POS_HIGH, POS_LOW);
+    _seeking = false;
   }
 
   void setup() {
@@ -81,119 +88,143 @@ class DriveUnit {
     digitalWrite(_pinDis, LOW);
   }
 
-  String channelName() {
+  String getChannelName() {
     return _channelName;
   }
-  
-  void seek(float target) {
-    // Inside this object we use potentiometer coordinates. Outside,
-    // we use the range [0.0, 1.0]
-    float t = constrain(target, 0.0, 1.0);
-    _target = t;
-    _seeking = seeking(t);
-  }
 
-  float target() {
+  int getTarget() {
     return _target;
   }
 
-  float pos() {
-    return (_vals[0] + _vals[1] + _vals[2]) / 3;
+  int getRaw() {
+    return _raw;
+  }
+  
+  int getPos() {
+    return _pos;
   }
 
-  void update() {
-    ++_counter;
+  int getSmoothedPos() {
+    return _smoothedPos;
+  }
 
-    // TODO: Do we still need to bother with this smoothing?
-    int aval = analogRead(_pinPos);
-    _vals[_counter % HISTORY] = aval;
+  int getDrive() {
+    return _drive;
+  }
 
-    int val = 0;
-    for (int i = 0; i < HISTORY; ++i) {
-      val += _vals[i];
-    }
-    val /= HISTORY;
+  void seek(int target) {
+    // Inside this object we use potentiometer coordinates. Outside,
+    // we use the range [0.0 100]
+    _priorTarget = _target;
+    _target = constrain(target, 0, 100);
+    _targetSetAt = millis();
+    _seeking = true;
+  }
 
-    int diff = abs(_seeking - val);
 
-    int drive = 0;
+  // Poor man's PWM. I may have to ditch this for something like an
+  // LED controller. Drive is in the range -100 to 100. The higher it
+  // is, the more time the signal spends low.
+  void setDrive(int drive)
+  {
+    _drive = drive;
+    static const int pwmFrequency = 500; // Hz
+    static const int pwmPeriod = 1000000 / pwmFrequency; // uSec
 
-    int fullSpeedDiff = 30;
-    int fullSpeedDrive = 250;
-    int zeroSpeedDiff = 5;
-    int minApproachSpeed = 100;
-
-    boolean uphill =
-      ((val < _seeking) && (_direction == Left)) ||
-      ((val > _seeking) && (_direction == Right));
-    boolean downhill = !uphill;
-
-    if (downhill) {
-      fullSpeedDiff = 100;
-      //zeroSpeedDiff = (int) (zeroSpeedDiff * 0.5F);
-      minApproachSpeed = 0;
-    }
-
-    if (diff > fullSpeedDiff) {
-      drive = fullSpeedDrive;
-    }
-    else if (diff > zeroSpeedDiff) {
-      drive = map(diff, zeroSpeedDiff, fullSpeedDiff, minApproachSpeed, fullSpeedDrive);
-    }
-
-    debug(_channelName);
-    debug(" > V");
-    debug(val);
-    debug(" (");
-    debug(aval);
-    debug(" : ");
-    debug(_vals[0]);
-    debug(", ");
-    debug(_vals[1]);
-    debug(", ");
-    debug(_vals[2]);
-    debug(") => S");
-    debug(_seeking);
-    debug(" = D");
-    debug(diff);
-    debug(" : Dr");
-    debug(drive);
-
+    int transition = map(abs(drive), 0, 100, 0, pwmPeriod);
+    
+    // TODO: Do we want to braking all the time?
     if (drive == 0) {
       digitalWrite(_pinEn, HIGH);
       digitalWrite(_pinRPWM, HIGH);
       digitalWrite(_pinLPWM, HIGH);
     }
-    else if (uphill) {
+    else if (drive < 0) {
       digitalWrite(_pinEn, HIGH);
       digitalWrite(_pinLPWM, HIGH);
 
-      if ((micros() % 255) < drive) {
+      if ((micros() % pwmPeriod) < transition) {
         digitalWrite(_pinRPWM, LOW);
       }
       else {
         digitalWrite(_pinRPWM, HIGH);
       }
-
-      //analogWrite(_pinRPWM, (250 - drive));
-      debug("L");
     }
     else {
       digitalWrite(_pinEn, HIGH);
       digitalWrite(_pinRPWM, HIGH);
 
-      if ((micros() % 255) < drive) {
+      if ((micros() % pwmPeriod) < transition) {
         digitalWrite(_pinLPWM, LOW);
       }
       else {
         digitalWrite(_pinLPWM, HIGH);
       }
-
-      //analogWrite(_pinLPWM, (250 - drive));
-      debug("R");
     }
 
-    debug();
+  }
+
+  void update() {
+    // Everything is normalized to the range either 0-100 or -100-100,
+    // depending on which makes more sense.
+    
+    ++_counter;
+
+    // The pot position normalized into the range 0 (all the way up)
+    // to 100 (all the way down).
+    _raw = analogRead(_pinPos);
+    _pos = map(_raw, POS_HIGH, POS_LOW, 0, 100);
+
+    int diff = _target - _pos;
+    int smoothing; // Range 0-100: how much we smooth out the current
+                   // value with previous values. 100 completely
+                   // ignors the current value. 0 uses exactly the
+                   // current value.
+
+    if (abs(diff) < 4) {
+      _seeking = false;
+    }
+
+    if (_seeking) {
+      smoothing = 50;
+    }
+    else {
+      smoothing = 90;
+    }
+    
+    _smoothedPos = ((_smoothedPos * smoothing) + (_pos * (100 - smoothing))) / 100;
+
+    // Smoothly transition to a new target
+    long targetAge = millis() - _targetSetAt;
+    static const int targetDelay = 1000; // ms
+    int currentTarget = 0;
+    if (targetAge < targetDelay) {
+      currentTarget = map(targetAge, 0, targetDelay, _priorTarget, _target);
+    }
+    else {
+      currentTarget = _target;
+    }
+    
+    int smoothedDiff = currentTarget - _smoothedPos;
+
+    int drive = 0;
+    if (smoothedDiff < -12) {
+      drive = -40;
+    }
+    else if (smoothedDiff < -2) {
+      drive = map(smoothedDiff, -12, -2, -40, -25);
+    }
+    else if (smoothedDiff < 3) {
+      drive = 0;
+    }
+    else if (smoothedDiff < 30) {
+      drive = map (smoothedDiff, 2, 30, 15, 40);
+    }
+    else {
+      drive = 40;
+    }
+    
+    setDrive(drive);
   }
 
 };
@@ -201,7 +232,7 @@ class DriveUnit {
 DriveUnit* driveUnits[] = {
   //            Ch,   En, Dis, RPWM, LPWM, pos, dir
   new DriveUnit("BL",  2,   4,    3,   11,   0, Left),
-  new DriveUnit("BR", 10,   8,    9,    7,   1, Left)
+  new DriveUnit("BR", 10,   8,    9,    7,   1, Right)
   //new DriveUnit("BR", 7, 8, 9, 10, 1)
   /* TODO: Others, although we only have seven PWM outputs, and we
      need 8 to drive four motors. One possibility is to use digital
@@ -213,33 +244,34 @@ DriveUnit* driveUnits[] = {
 const int driveUnitCount = sizeof(driveUnits)/sizeof(DriveUnit*);
 
 void dispatchCommand(String command) {
-  Serial.print("Parsing ");
-  Serial.println(command);
+  debug("Parsing ");
+  debug(command);
+  debug();
 
   if (command.startsWith("M ")) {
     String args = command.substring(2);
-    if (_logLevel >= Debug) {
-      Serial.println("Command is move");
-      Serial.print("args: ");
-      Serial.println(args);
-    }
+    debug("Command is move");
+    debug("args: ");
+    debug(args);
+    debug();
+
     for (int i = 0; i < driveUnitCount; ++i) {
       DriveUnit* unit = driveUnits[i];
-      String channel = unit->channelName();
+      String channel = unit->getChannelName();
       String val = args.substring(channel.length());
-      if (_logLevel >= Debug) {
-        Serial.print("Checking ");
-        Serial.println(channel);
-        Serial.print("val: '");
-        Serial.print(val);
-        Serial.println("'");
-      }
+      debug("Checking ");
+      debug(channel);
+      debug();
+      debug("val: '");
+      debug(val);
+      debug("'");
+      debug();
+
       if (args.startsWith(channel) && val.startsWith(" ")) {
-        float target = val.substring(1).toFloat();
-        if (_logLevel >= Debug) {
-          Serial.print("New target: ");
-          Serial.println(target);
-        }
+        int target = val.substring(1).toInt();
+        debug("New target: ");
+        debug(target);
+        debug();
         unit->seek(target);
         break;
       }
@@ -248,14 +280,11 @@ void dispatchCommand(String command) {
   else if (command.equals("Q")) {
     for (int i = 0; i < driveUnitCount; ++i) {
       DriveUnit* unit = driveUnits[i];
-      Serial.print(unit->channelName());
+      Serial.print(unit->getChannelName());
       Serial.print(" at ");
-      Serial.print(unit->pos());
+      Serial.print(unit->getPos());
       Serial.print(" seeking ");
-      Serial.print(unit->seeking(unit->target()));
-      Serial.print(" (");
-      Serial.print(unit->target());
-      Serial.print(") ");
+      Serial.print(unit->getTarget());
       Serial.println();
     }
   }
@@ -291,7 +320,7 @@ void setup() {
 
 void loop() {
   static String pending = "";
-  static int vals[] = { 950, 950, 950 };
+  static long lastDebug = millis();
 
   while (Serial.available() > 0) {
     int inChar = Serial.read();
@@ -308,5 +337,21 @@ void loop() {
   for (int i = 0; i < driveUnitCount; ++i) {
     driveUnits[i]->update();
   }
+
+  if ((millis() - lastDebug) > 500) {
+    for (int i = 0; i < driveUnitCount; ++i) {
+      DriveUnit* unit = driveUnits[i];
+      char str[512];
+      sprintf(str, "%s Target:%02d Raw:%04d Pos:%02d (Smooth:%02d) Drive:%02d",
+              unit->getChannelName().c_str(),
+              unit->getTarget(), unit->getRaw(), unit->getPos(),
+              unit->getSmoothedPos(), unit->getDrive());
+      debug(str);
+      debug();
+      lastDebug = millis();
+    }
+  }
+  
+
 
 }
